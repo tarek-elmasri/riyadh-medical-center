1204c22453f67ad64fd8b8868fe887a60c8e3d7f
tarek-elmasri <tarek.elmasri201@gmail.com>
2023-07-17 20:02:05 +0300

    [fix] dates

--- app/(root)/dashboard/appointments/page.tsx
+++ app/(root)/dashboard/appointments/page.tsx
@@ -1,16 +1,20 @@
 import DashboardPageHeader from "@/components/dashboard/dashboard-page-header";
 import Controler from "./Controler";
 import { getDoctors } from "@/app/actions/getDoctors";
+import { standardDate } from "@/lib/utils";
 
 const AppointmentsPage = async () => {
   const doctors = await getDoctors();
-
+  const serverDate = new Date();
+  const standard = standardDate(new Date());
   return (
     <div>
       <DashboardPageHeader
         label="الحجوزات"
         addNewURL="/dashboard/appointments/new"
       />
+      {`server date: ${serverDate}`}
+      {`standard date: ${standard}`}
 
       <Controler doctors={doctors} />
     </div>
--- lib/utils.ts
+++ lib/utils.ts
@@ -12,10 +12,9 @@ export function cn(...inputs: ClassValue[]) {
 export const standardDate = (date: Date) => new Date(date.setHours(0, 0, 0, 0));
 
 export const todayInKSA = () => {
-  // const local = new Date();
-  // const timezoneOffset = local.getTimezoneOffset();
-  // const utc = new Date(local.getTime() - timezoneOffset * 6000);
-  // // const ksaOffset = 3 * 60 * 60 * 1000;
-  // return utc;
-  return new Date();
+  const local = new Date();
+  const timezoneOffset = local.getTimezoneOffset();
+  const utc = new Date(local.getTime() - timezoneOffset * 6000);
+  const ksaOffset = 3 * 60 * 60 * 1000;
+  return new Date(utc.getTime() + ksaOffset);
 };
