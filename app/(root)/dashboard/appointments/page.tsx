89baa7c2de1543543ce22f9e312bfa5a5b8d6f91
tarek-elmasri <tarek.elmasri201@gmail.com>
2023-07-17 20:25:01 +0300

    [fix] dates

--- app/(root)/dashboard/appointments/page.tsx
+++ app/(root)/dashboard/appointments/page.tsx
@@ -1,12 +1,14 @@
 import DashboardPageHeader from "@/components/dashboard/dashboard-page-header";
 import Controler from "./Controler";
 import { getDoctors } from "@/app/actions/getDoctors";
-import { standardDate } from "@/lib/utils";
+import { standardDate, todayInKSA } from "@/lib/utils";
 
 const AppointmentsPage = async () => {
   const doctors = await getDoctors();
   const serverDate = new Date();
   const standard = standardDate(new Date());
+  const timeInKSA = todayInKSA();
+  const standardInKSA = standardDate(todayInKSA());
   return (
     <div>
       <DashboardPageHeader
@@ -15,7 +17,8 @@ const AppointmentsPage = async () => {
       />
       {`server date: ${serverDate}`}
       {`standard date: ${standard}`}
-
+      {`in KSA: ${timeInKSA}`}
+      {`standard in KSA ${standardInKSA}`}
       <Controler doctors={doctors} />
     </div>
   );
