777fc5098f5fd8379b9cd460171552d61a16fdd5
tarek-elmasri <tarek.elmasri201@gmail.com>
2023-07-17 19:42:08 +0300

    [fix] dates

--- lib/utils.ts
+++ lib/utils.ts
@@ -12,9 +12,10 @@ export function cn(...inputs: ClassValue[]) {
 export const standardDate = (date: Date) => new Date(date.setHours(0, 0, 0, 0));
 
 export const todayInKSA = () => {
-  const local = new Date();
-  const timezoneOffset = local.getTimezoneOffset();
-  const utc = new Date(local.getTime() - timezoneOffset * 6000);
-  // const ksaOffset = 3 * 60 * 60 * 1000;
-  return utc;
+  // const local = new Date();
+  // const timezoneOffset = local.getTimezoneOffset();
+  // const utc = new Date(local.getTime() - timezoneOffset * 6000);
+  // // const ksaOffset = 3 * 60 * 60 * 1000;
+  // return utc;
+  return new Date();
 };
