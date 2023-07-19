54961d60588e1306bff51cea34bb9ab4cf2c5f0a
tarek-elmasri <tarek.elmasri201@gmail.com>
2023-07-20 00:56:51 +0300

    [refactor] replace main dashboard page for counters page

--- app/(root)/dashboard/page.tsx
+++ app/(root)/dashboard/page.tsx
@@ -1,5 +1,21 @@
-const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+import { getPusherList } from "@/app/actions/pusher";
+import { getDoctorsCounterList } from "@/app/actions/getDoctors";
+import CounterClient from "@/components/counters/counter-client";
+import CounterList from "@/components/counters/counter-list";
+
+const CounterPage = async () => {
+  const currentPusherList = await getPusherList();
+
+  const doctorsList = await getDoctorsCounterList();
+
+  return (
+    <div>
+      <CounterList initialList={doctorsList} />
+      <div className="mt-6 flex flex-col md:flex-row justify-center items-center flex-wrap gap-12">
+        <CounterClient initialData={currentPusherList} />
+      </div>
+    </div>
+  );
 };
 
-export default DashboardPage;
+export default CounterPage;
