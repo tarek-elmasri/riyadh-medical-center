68f1fd4fc2cf05c2fd47ffc9ae19fd188749a36d
tarek-elmasri <tarek.elmasri201@gmail.com>
2023-07-19 13:50:20 +0300

    [fix] pusher subscriptions and events

--- lib/pusher.ts
+++ lib/pusher.ts
@@ -1,12 +1,7 @@
 import Pusher from "pusher";
 import PusherClient from "pusher-js";
 
-declare global {
-  var pusherServer: Pusher | undefined;
-  var pusherClient: PusherClient | undefined;
-}
-
-const newPusher = new Pusher({
+const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_SECRET!,
@@ -14,19 +9,11 @@ const newPusher = new Pusher({
   useTLS: true,
 });
 
-const pusher = globalThis.pusherServer || newPusher;
-
-if (process.env.NODE_ENV !== "production") globalThis.pusherServer = newPusher;
-
-// async () => await pusher.trigger("counters", "list", []);
-
-const newPusherClient = new PusherClient(
+export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   {
     cluster: "ap2",
   }
 );
-
-export const pusherClient = globalThis.pusherClient || newPusherClient;
 
 export default pusher;
